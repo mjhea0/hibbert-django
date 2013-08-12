d87d6c3b6a93d43a3c88a5c823a49399411b4bbd
Michael Herman <hermanmu@gmail.com>
2013-08-11 21:33:44 -0500

    added requirements.txt

--- django_test/settings.py
+++ django_test/settings.py
@@ -188,14 +188,14 @@ try:
 except Exception as e:
     print e.message
 
-if not DEBUG:
-    AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
-    AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-    AWS_PRELOAD_METADATA = True
-    STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-    S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
-    STATIC_URL = S3_URL
-    ALLOWED_HOSTS = ['djanngo-hibbert.herokuapp.com', '127.0.0.1']
+# if not DEBUG:
+#     AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+#     AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+#     AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+#     AWS_PRELOAD_METADATA = True
+#     STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+#     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+#     S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+#     STATIC_URL = S3_URL
+#     ALLOWED_HOSTS = ['djanngo-hibbert.herokuapp.com', '127.0.0.1']
 
--- django_test/settings_new.py
+++ django_test/settings_new.py
@@ -170,10 +170,10 @@ DATABASES['default'] =  dj_database_url.config(default=os.environ.get('DATABASE_
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 #Storage on S3 settings are stored as os.environs to keep settings.py clean 
-# if not DEBUG:
-#     AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
-#     AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-#     AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-#     STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-#     S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
-#     STATIC_URL = S3_URL
+if not DEBUG:
+    AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+    AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+    STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+    S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+    STATIC_URL = S3_URL
