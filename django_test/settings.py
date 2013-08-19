3ceb9d0df378a7f4909b055062159f1bb1d54571
Michael Herman <hermanmu@gmail.com>
2013-08-18 22:53:58 -0500

    first

--- django_test/settings.py
+++ django_test/settings.py
@@ -189,19 +189,10 @@ try:
 except Exception as e:
     print e.message
 
-# if not DEBUG:
-#     AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
-#     AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-#     AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-#     AWS_PRELOAD_METADATA = True
-#     STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-#     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-#     S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
-#     STATIC_URL = S3_URL
     
 if not DEBUG:
     AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
-    AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+    AWS_ACCESS_KEY = os.environ['AWS_ACCESS_KEY']
     AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
     STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
     S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
